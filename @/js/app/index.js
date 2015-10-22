0192961e55797d3a4f9171ab323fcbe6e004f8c3
uikoo9 <uikoo9@qq.com>
2015-10-22 21:26:00 +0800

    js 模块化改装

--- @/js/app/index.js
+++ @/js/app/index.js
@@ -1,122 +1,129 @@
-// 初始化
-mui.init({
-	subpages : [qiao.h.normalPage('list')]
-});
-
-var main = null;
-var showMenu = false;
-var menu = null;
-var add = null;
-var detail = null;
-
-// 所有方法都放到这里
-mui.plusReady(function(){
-	setColor("#f7f7f7");
+define(function (require, exports, module) {
+    'use strict';
+    
+    // import
+    var qiao = require('../lib/qiao/qiao.js');
+    
+	// 初始化
+	mui.init({
+		subpages : [qiao.normalPage('list')]
+	});
+	
+	var main = null;
+	var showMenu = false;
+	var menu = null;
+	var add = null;
+	var detail = null;
+	
+	// 所有方法都放到这里
+	mui.plusReady(function(){
+		setColor("#f7f7f7");
+		
+		// 初始化数据库
+		initDb();
+		
+		// 侧滑菜单
+		main = qiao.indexPage();
+		var menuoptions = qiao.page('menu', {
+			styles : {
+				left:0,
+				width:'70%',
+				zindex:-1
+			}
+		});
+		menu = mui.preload(menuoptions);
+		qiao.on('.mui-icon-bars', 'tap', opMenu);
+		main.addEventListener('maskClick', opMenu);
+		mui.menu = opMenu;
+		
+		// 添加
+		add = mui.preload(qiao.normalPage('add'));
+		qiao.on('.adda', 'tap', showAdd);
+		qiao.on('.mui-icon-back', 'tap', hideAdd);
+		
+		// 详情
+		detail = mui.preload(qiao.normalPage('detail'));
+		
+		// 退出
+		mui.back = function(){
+			if($('.adda').is(':hidden')){
+				hideAdd();	
+			}else if(showMenu){
+				closeMenu();
+			}else{
+				qiao.exit();
+			}
+		};
+	});
 	
 	// 初始化数据库
-	initDb();
+	function initDb(){
+		var db = qiao.db();
+		qiao.update(db, 'create table if not exists t_plan_day_todo (id unique, plan_title, plan_content)');
+		qiao.update(db, 'create table if not exists t_plan_day_done (id unique, plan_title, plan_content)');
+	}
 	
-	// 侧滑菜单
-	main = qiao.h.indexPage();
-	var menuoptions = qiao.h.page('menu', {
-		styles : {
-			left:0,
-			width:'70%',
-			zindex:-1
-		}
-	});
-	menu = mui.preload(menuoptions);
-	qiao.on('.mui-icon-bars', 'tap', opMenu);
-	main.addEventListener('maskClick', opMenu);
-	mui.menu = opMenu;
-	
-	// 添加
-	add = mui.preload(qiao.h.normalPage('add'));
-	qiao.on('.adda', 'tap', showAdd);
-	qiao.on('.mui-icon-back', 'tap', hideAdd);
-	
-	// 详情
-	detail = mui.preload(qiao.h.normalPage('detail'));
-	
-	// 退出
-	mui.back = function(){
-		if($('.adda').is(':hidden')){
-			hideAdd();	
-		}else if(showMenu){
+	// menu
+	function opMenu(){
+		if(showMenu){
 			closeMenu();
 		}else{
-			qiao.h.exit();
+			openMenu();
 		}
-	};
-});
-
-// 初始化数据库
-function initDb(){
-	var db = qiao.h.db();
-	qiao.h.update(db, 'create table if not exists t_plan_day_todo (id unique, plan_title, plan_content)');
-	qiao.h.update(db, 'create table if not exists t_plan_day_done (id unique, plan_title, plan_content)');
-}
-
-// menu
-function opMenu(){
-	if(showMenu){
-		closeMenu();
-	}else{
-		openMenu();
 	}
-}
-function openMenu(){
-	if($('.adda').is(':visible')){
-		setColor("#333333");
-		menu.show('none', 0, function() {
-			main.setStyle({
-				mask: 'rgba(0,0,0,0.4)',
-				left: '70%',
-				transition: {
-					duration: 150
-				}
+	function openMenu(){
+		if($('.adda').is(':visible')){
+			setColor("#333333");
+			menu.show('none', 0, function() {
+				main.setStyle({
+					mask: 'rgba(0,0,0,0.4)',
+					left: '70%',
+					transition: {
+						duration: 150
+					}
+				});
+		
+				showMenu = true;
 			});
-	
-			showMenu = true;
-		});
-	}
-}
-function closeMenu(){
-	setColor("#f7f7f7");
-	main.setStyle({
-		mask: 'none',
-		left: '0',
-		transition: {
-			duration: 100
 		}
-	});
+	}
+	function closeMenu(){
+		setColor("#f7f7f7");
+		main.setStyle({
+			mask: 'none',
+			left: '0',
+			transition: {
+				duration: 100
+			}
+		});
+		
+		showMenu = false;
+		setTimeout(function() {
+			menu.hide();
+		}, 300);
+	}
 	
-	showMenu = false;
-	setTimeout(function() {
-		menu.hide();
-	}, 300);
-}
-
-// showAdd
-function showAdd(){
-	showBackBtn();
-	qiao.h.show('add', 'slide-in-bottom', 300);
-}
-function hideAdd(){
-	hideBackBtn();
-	qiao.h.getPage('add').hide();
-	qiao.h.getPage('detail').hide();
-}
-function showBackBtn(){
-	$('.menua').removeClass('mui-icon-bars').addClass('mui-icon-back');
-	$('.adda').hide();
-}
-function hideBackBtn(){
-	$('.menua').removeClass('mui-icon-back').addClass('mui-icon-bars');
-	$('.adda').show();
-}
-
-// set color
-function setColor(color){
-	if(mui.os.ios && color) plus.navigator.setStatusBarBackground(color);
-}
+	// showAdd
+	function showAdd(){
+		showBackBtn();
+		qiao.show('add', 'slide-in-bottom', 300);
+	}
+	function hideAdd(){
+		hideBackBtn();
+		qiao.getPage('add').hide();
+		qiao.getPage('detail').hide();
+	}
+	function showBackBtn(){
+		$('.menua').removeClass('mui-icon-bars').addClass('mui-icon-back');
+		$('.adda').hide();
+	}
+	function hideBackBtn(){
+		$('.menua').removeClass('mui-icon-back').addClass('mui-icon-bars');
+		$('.adda').show();
+	}
+	
+	// set color
+	function setColor(color){
+		if(mui.os.ios && color) plus.navigator.setStatusBarBackground(color);
+	}
+});
--- @/js/app/menu.js
+++ @/js/app/menu.js
@@ -1,53 +1,59 @@
-// 初始化
-mui.init({
-	keyEventBind : {
-		backbutton : false
+define(function (require, exports, module) {
+    'use strict';
+    
+    // import
+    var qiao = require('../lib/qiao/qiao.js');
+    
+	// 初始化
+	mui.init({
+		keyEventBind : {
+			backbutton : false
+		}
+	});
+	
+	// 所有的方法都放到这里
+	mui.plusReady(function(){
+		initDoneList();
+		
+		// 添加已完成事项
+		window.addEventListener('doneItem', doneItemHandler);
+	});
+	
+	// 初始化待办事项
+	function initDoneList(){
+		var $ul = $('#donelist').empty();
+		qiao.query(qiao.db(), 'select * from t_plan_day_done order by id desc', function(res){
+			for (i = 0; i < res.rows.length; i++) {
+				$ul.append(genLi(res.rows.item(i).plan_title));
+			}
+	
+			showList($ul);
+		});
 	}
-});
-
-
-// 所有的方法都放到这里
-mui.plusReady(function(){
-	initDoneList();
+	function genLi(title){
+		return '<li class="mui-table-view-cell">' + title + '</li>';
+	}
+	function showList(ul){
+		if(ul.find('li').size() > 0 &&  ul.is(':hidden')) ul.show();
+	}
 	
 	// 添加已完成事项
-	window.addEventListener('doneItem', doneItemHandler);
-});
-
-// 初始化待办事项
-function initDoneList(){
-	var $ul = $('#donelist').empty();
-	qiao.h.query(qiao.h.db(), 'select * from t_plan_day_done order by id desc', function(res){
-		for (i = 0; i < res.rows.length; i++) {
-			$ul.append(genLi(res.rows.item(i).plan_title));
-		}
-
-		showList($ul);
-	});
-}
-function genLi(title){
-	return '<li class="mui-table-view-cell">' + title + '</li>';
-}
-function showList(ul){
-	if(ul.find('li').size() > 0 &&  ul.is(':hidden')) ul.show();
-}
-
-// 添加已完成事项
-function doneItemHandler(event){
-	var todoId = event.detail.todoId;
-
-	var db = qiao.h.db();
-	qiao.h.query(db, 'select * from t_plan_day_todo where id=' + todoId, function(res){
-		if(res.rows.length > 0){
-			var data = res.rows.item(0);
-			
-			qiao.h.query(db, 'select max(id) mid from t_plan_day_done', function(res1){
-				$('#donelist').prepend('<li class="mui-table-view-cell>test</li>').prepend(genLi(data.plan_title)).show();
+	function doneItemHandler(event){
+		var todoId = event.detail.todoId;
+	
+		var db = qiao.db();
+		qiao.query(db, 'select * from t_plan_day_todo where id=' + todoId, function(res){
+			if(res.rows.length > 0){
+				var data = res.rows.item(0);
 				
-				var id = (res1.rows.item(0).mid) ? res1.rows.item(0).mid : 0;
-				qiao.h.update(db, 'insert into t_plan_day_done (id, plan_title, plan_content) values (' + (id+1) + ', "' + data.plan_title + '", "' + data.plan_content + '")');
-				qiao.h.update(db, 'delete from t_plan_day_todo where id=' + todoId);
-			});
-		}
-	});
-}
+				qiao.query(db, 'select max(id) mid from t_plan_day_done', function(res1){
+					$('#donelist').prepend('<li class="mui-table-view-cell>test</li>').prepend(genLi(data.plan_title)).show();
+					
+					var id = (res1.rows.item(0).mid) ? res1.rows.item(0).mid : 0;
+					qiao.update(db, 'insert into t_plan_day_done (id, plan_title, plan_content) values (' + (id+1) + ', "' + data.plan_title + '", "' + data.plan_content + '")');
+					qiao.update(db, 'delete from t_plan_day_todo where id=' + todoId);
+				});
+			}
+		});
+	}
+});
